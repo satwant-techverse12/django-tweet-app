aea41f9dc33c69b6e12e9997d6091f27d5145821
satwant-techverse12 <satwant@techversyssolutions.com>
2026-04-28 14:49:38 +0530

    fixed deployment issues

--- chaiheadq/settings.py
+++ chaiheadq/settings.py
@@ -43,6 +43,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',  # must be just after security
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -123,8 +124,11 @@ MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 STATIC_URL ='static/'
+STATIC_ROOT = BASE_DIR / 'staticfiles'
 STATICFILES_DIRS =[os.path.join(BASE_DIR,'static')]
 
 LOGIN_URL = '/accounts/login'
 LOGIN_REDIRECT_URL ='/tweet/'
 LOGOUT_REDIRECT_URL ='/tweet/'
+ALLOWED_HOSTS = ['*']  # temporary for deployment
+
